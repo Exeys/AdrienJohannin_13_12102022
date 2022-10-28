2eb6dae07fa90499342946c1dd5720ddb732df50
Adrien <47904625+Exeys@users.noreply.github.com>
2022-10-28 16:38:15 +0200

    updated header on state

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link, Navigat, useNavigate } from "react-router-dom"
 import './Header.scss'
 import logo from '../../assets/img/argentBankLogo.png'
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
+    const state = useSelector((state) => state)
+    const dispatch = useDispatch()
+    const isLogged = state.logged
+    const nav = useNavigate()
+
+    const logOut = (event) => {
+        event.preventDefault()
+        dispatch({ type: 'LOGOUT' })
+        nav("/")
+    }
+
     return (
         <nav className="main-nav">
             <Link to="/" className="main-nav-logo">
@@ -14,12 +26,26 @@ const Header = () => {
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
-            <div>
-                <Link to="/signin" className="main-nav-item">
-                        <i className="fa fa-user-circle"></i>
+            {!isLogged ? (
+                <div>
+                    <Link to="/signin" className="main-nav-item">
+                        <i className='fa fa-sign-in'></i>
                         Sign In
-                </Link>
-            </div>
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/user" className="main-nav-item" >
+                        <i className="fa fa-user-circle"></i>
+                        {state.firstName}
+                    </Link>
+                    <Link to="/" className="main-nav-item" onClick={logOut} >
+                        <i className='fa fa-sign-out'></i>
+                        Sign Out
+                    </Link>
+                </div>
+            )}
+
         </nav>
     )
 }
